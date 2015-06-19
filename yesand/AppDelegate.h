487029f2297d52c64e838a30e80a7071f407dc3c
Husein Kareem <huseinkareem@gmail.com>
2015-06-19 17:02:32 -0500

    Initial Commit

---
+++ yesand/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  yesand
+//
+//  Created by Husein Kareem on 6/19/15.
+//  Copyright (c) 2015 Meduse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ yesand/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yesand
+//
+//  Created by Husein Kareem on 6/19/15.
+//  Copyright (c) 2015 Meduse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
