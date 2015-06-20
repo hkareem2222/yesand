791d7a52a032c719b55b6c2e202aac6749a14aab
Husein Kareem <huseinkareem@gmail.com>
2015-06-20 09:16:10 -0500

    user auth check on lauch

--- yesand/AuthViewController.m
+++ yesand/AuthViewController.m
@@ -14,15 +14,56 @@
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
 @property (weak, nonatomic) IBOutlet UIButton *signUpButton;
 @property (weak, nonatomic) IBOutlet UIButton *logInButton;
+@property Firebase *myRootRef;
 @end
 
 @implementation AuthViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.myRootRef = [[Firebase alloc] initWithUrl:@"https://yesand.firebaseio.com"];
+    self.signUpButton.layer.cornerRadius = 10;
+    [self.myRootRef observeAuthEventWithBlock:^(FAuthData *authData) {
+        if (authData) {
+            [self performSegueWithIdentifier:@"AuthToHome" sender:self];
+            NSLog(@"%@", authData);
+        } else {
+            NSLog(@"no user logged in");
+        }
+    }];
 }
-- (IBAction)onSignUpButtonPressed:(id)sender {
+
+#pragma mark - Button Actions 
+
+- (IBAction)onSignUpButtonPressed:(UIButton *)button {
+    if ([button.titleLabel.text isEqualToString:@"Sign up"]) {
+        [self.myRootRef createUser:self.emailField.text password:self.passwordField.text
+          withValueCompletionBlock:^(NSError *error, NSDictionary *result) {
+              if (error) {
+                  NSLog(@"error: %@", [error localizedDescription]);
+              } else {
+                  NSString *uid = [result objectForKey:@"uid"];
+                  NSLog(@"Successfully created user account with uid: %@", uid);
+              }
+          }];
+    } else {
+        [self.myRootRef authUser:self.emailField.text password:self.passwordField.text
+            withCompletionBlock:^(NSError *error, FAuthData *authData) {
+      if (error) {
+          NSLog(@"error logging in: %@", [error localizedDescription]);
+      } else {
+          NSLog(@"logged in");
+      }
+  }];
+    }
 }
-- (IBAction)onLoginButtonPressed:(id)sender {
+- (IBAction)onLoginButtonPressed:(UIButton *)button {
+    if ([button.titleLabel.text isEqualToString:@"Log in"]) {
+        [button setTitle:@"Sign up" forState:UIControlStateNormal];
+        [self.signUpButton setTitle:@"Log in" forState:UIControlStateNormal];
+    } else {
+        [button setTitle:@"Log in" forState:UIControlStateNormal];
+        [self.signUpButton setTitle:@"Sign up" forState:UIControlStateNormal];
+    }
 }
 @end
