916953119b53ae1dee19cf092057b74969a041fc
Tom Carmona <thomascarmona@gmail.com>
2015-06-24 14:54:40 -0500

    finished star rating animation

--- yesand/RatingViewController.m
+++ yesand/RatingViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "RatingViewController.h"
+#import "RateView.h"
 
 
-@interface RatingViewController ()
+@interface RatingViewController () <RateViewDelegate>
+
+@property (weak, nonatomic) IBOutlet RateView *rateView;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+
 
 @end
 
@@ -22,12 +27,47 @@
     self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:255/255.0 green:40/255.0 blue:40/255.0 alpha:1.0];
 
     self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:255/255.0 green:255/255.0 blue:255/255.0 alpha:1.0];
+
+    self.rateView.notSelectedImage = [UIImage imageNamed:@"graystar.png"];
+    self.rateView.halfSelectedImage = [UIImage imageNamed:@"star.png"];
+    self.rateView.fullSelectedImage = [UIImage imageNamed:@"star.png"];
+    self.rateView.rating = 0;
+    self.rateView.editable = YES;
+    self.rateView.maxRating = 5;
+    self.rateView.delegate = self;
+
+    [self rateView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)rateView:(RateView *)rateView ratingDidChange:(float)rating {
+
+
+    if (self.rateView.rating == 0) {
+        self.statusLabel.text = [NSString stringWithFormat:@""];
+    }
+    if (self.rateView.rating == 1) {
+        self.statusLabel.text = [NSString stringWithFormat:@"1 - Not cool, bro"];
+    }
+    if (self.rateView.rating == 2) {
+    self.statusLabel.text = [NSString stringWithFormat:@"2 - Meh..."];
+    }
+    if (self.rateView.rating == 3) {
+    self.statusLabel.text = [NSString stringWithFormat:@"3 - Not shabby, not great"];
+    }
+    if (self.rateView.rating == 4) {
+    self.statusLabel.text = [NSString stringWithFormat:@"4 - I lol'd"];
+    }
+    if (self.rateView.rating == 5) {
+        self.statusLabel.text = [NSString stringWithFormat:@"5 - Comedy so good I just pooped myself"];
+    }
 }
+
+
+
+
+
+//    self.statusLabel.text = [NSString stringWithFormat:@"Rating: %f", self.rateView.rating];
+//}
 
 /*
 #pragma mark - Navigation
