648f74e654a4b9ed9a7fc6a00a3f01b56594a343
Husein Kareem <huseinkareem@gmail.com>
2015-06-28 13:18:49 -0500

    audienceVC view changes

--- yesand/AudienceViewController.m
+++ yesand/AudienceViewController.m
@@ -15,6 +15,13 @@
 @property (weak, nonatomic) IBOutlet UILabel *sceneTitleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *characterOneLabel;
 @property (weak, nonatomic) IBOutlet UILabel *characterTwoLabel;
+@property NSNumber *laughs;
+@property Firebase *scenesConvo;
+@property (weak, nonatomic) IBOutlet UILabel *laughsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *chatLabel1;
+@property (weak, nonatomic) IBOutlet UILabel *chatLabel2;
+@property (weak, nonatomic) IBOutlet UILabel *chatLabel3;
+@property (weak, nonatomic) IBOutlet UILabel *chatLabel4;
 @end
 
 @implementation AudienceViewController
@@ -24,20 +31,41 @@
     self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
     NSLog(@"selected scene id %@", self.sceneID);
     NSString *sceneURL = [NSString stringWithFormat:@"https://yesand.firebaseio.com/scenes/%@", self.sceneID];
-    Firebase *scenesConvo = [[Firebase alloc] initWithUrl:sceneURL];
+    self.scenesConvo = [[Firebase alloc] initWithUrl:sceneURL];
 
-    [scenesConvo observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+    [self.scenesConvo observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
         if (![snapshot.value[@"messages"] isEqual:[NSNull null]]) {
             self.messages = snapshot.value[@"messages"];
             self.sceneTitleLabel.text = snapshot.value[@"topicName"];
             self.characterOneLabel.text = snapshot.value[@"characterOne"];
             NSLog(@"%@", snapshot.value);
             self.characterTwoLabel.text = snapshot.value[@"characterTwo"];
+            self.laughs = snapshot.value[@"laughs"];
+            self.laughsLabel.text = self.laughs.stringValue;
             [self.tableView reloadData];
         }
     } withCancelBlock:^(NSError *error) {
         NSLog(@"%@", error.description);
     }];
+
+    UITapGestureRecognizer *singleFingerTap =
+    [[UITapGestureRecognizer alloc] initWithTarget:self
+                                            action:@selector(handleSingleTap:)];
+    [self.tableView addGestureRecognizer:singleFingerTap];
+}
+
+//The event handling method
+- (void)handleSingleTap:(UITapGestureRecognizer *)recognizer {
+    NSLog(@"tapped");
+//    CGPoint location = [recognizer locationInView:[recognizer.view superview]];
+    NSInteger laughsInt = self.laughs.integerValue;
+    laughsInt += 1;
+    self.laughs = [NSNumber numberWithInteger:laughsInt];
+    NSDictionary *sceneLaughs = @{
+                                  @"laughs": self.laughs
+                                  };
+    [self.scenesConvo updateChildValues:sceneLaughs];
+    self.laughsLabel.text = self.laughs.stringValue;
 }
 
 #pragma mark - TableView
