0b17a0be90758be7283b2e39e0583204fc8e8c50
jdivittorio3 <jdivittorio3@gmail.com>
2015-07-01 22:54:50 -0500

    commiting to pull

--- yesand/AudienceViewController.m
+++ yesand/AudienceViewController.m
@@ -20,14 +20,7 @@
 @property NSNumber *laughs;
 @property Firebase *scenesConvo;
 @property (weak, nonatomic) IBOutlet UILabel *laughsLabel;
-@property (weak, nonatomic) IBOutlet UILabel *chatLabel1;
-@property (weak, nonatomic) IBOutlet UILabel *chatLabel2;
-@property (weak, nonatomic) IBOutlet UILabel *chatLabel3;
-@property (weak, nonatomic) IBOutlet UILabel *chatLabel4;
-@property (weak, nonatomic) IBOutlet UITextField *messageField;
-@property (weak, nonatomic) IBOutlet UIView *audienceChatView;
 @property NSInteger labelCount;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textFieldBottomLayout;
 @end
 
 @implementation AudienceViewController
@@ -36,25 +29,14 @@
     [super viewDidLoad];
 
     //views setup
-    self.chatLabel1.layer.cornerRadius = 5;
-    self.chatLabel2.layer.cornerRadius = 5;
-    self.chatLabel3.layer.cornerRadius = 5;
-    self.chatLabel4.layer.cornerRadius = 5;
-    self.chatLabel1.clipsToBounds = YES;
-    self.chatLabel2.clipsToBounds = YES;
-    self.chatLabel3.clipsToBounds = YES;
-    self.chatLabel4.clipsToBounds = YES;
     self.labelCount = 0;
-    self.chatLabel1.alpha = 0.0;
-    self.chatLabel2.alpha = 0.0;
-    self.chatLabel3.alpha = 0.0;
-    self.chatLabel4.alpha = 0.0;
     self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:255/255.0 green:255/255.0 blue:255/255.0 alpha:1.0];
     NSDictionary *attrDict = @{
                                NSFontAttributeName : [UIFont fontWithName:@"AppleGothic" size:21.0],
                                NSForegroundColorAttributeName : [UIColor whiteColor]
                                };
     self.navigationController.navigationBar.titleTextAttributes = attrDict;
+    self.sceneTitleLabel.font = [UIFont fontWithName: @"AppleGothic" size: 15.0];
 
     //Scene Setup
     NSString *sceneURL = [NSString stringWithFormat:@"https://yesand.firebaseio.com/scenes/%@", self.sceneID];
@@ -92,8 +74,8 @@
                                             action:@selector(handleSingleTap:)];
     [self.tableView addGestureRecognizer:singleFingerTap];
 
-    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
-    [center addObserver:self selector:@selector(keyboardOnScreen:) name:UIKeyboardWillShowNotification object:nil];
+//    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+//    [center addObserver:self selector:@selector(keyboardOnScreen:) name:UIKeyboardWillShowNotification object:nil];
 
     self.tableView.separatorColor = [UIColor clearColor];
     self.tableView.rowHeight = UITableViewAutomaticDimension;
@@ -112,46 +94,46 @@
     self.laughsLabel.text = self.laughs.stringValue;
 }
 
-#pragma mark - Keyboard Animation
+//#pragma mark - Keyboard Animation
+//
+//-(void)keyboardOnScreen:(NSNotification *)notification
+//{
+//    NSDictionary *info  = notification.userInfo;
+//    NSValue      *value = info[UIKeyboardFrameEndUserInfoKey];
+//
+//    CGRect rawFrame      = [value CGRectValue];
+//    CGRect keyboardFrame = [self.view convertRect:rawFrame fromView:nil];
+//
+//    //make sure to create the outlet for the textfieldbottomlayout//
+//    self.textFieldBottomLayout.constant = keyboardFrame.size.height - 50;
+//}
 
--(void)keyboardOnScreen:(NSNotification *)notification
-{
-    NSDictionary *info  = notification.userInfo;
-    NSValue      *value = info[UIKeyboardFrameEndUserInfoKey];
-
-    CGRect rawFrame      = [value CGRectValue];
-    CGRect keyboardFrame = [self.view convertRect:rawFrame fromView:nil];
-
-    //make sure to create the outlet for the textfieldbottomlayout//
-    self.textFieldBottomLayout.constant = keyboardFrame.size.height - 50;
-}
-
-# pragma mark - Send Button
-
-- (IBAction)onSendButtonPressed:(id)sender {
-    NSArray *labels = @[self.chatLabel1, self.chatLabel2, self.chatLabel3, self.chatLabel4];
-    UILabel *label;
-    if (self.labelCount < labels.count) {
-        label = labels[self.labelCount];
-    }
-    if (self.labelCount == 0) {
-        self.labelCount += 1;
-        label.text = self.messageField.text;
-        label.alpha = 1.0;
-    } else if (self.labelCount == 1) {
-        self.labelCount += 1;
-        label.text = self.messageField.text;
-        label.alpha = 1.0;
-    } else if (self.labelCount == 2) {
-        self.labelCount += 1;
-        label.text = self.messageField.text;
-        label.alpha = 1.0;
-    } else if (self.labelCount == 3) {
-        self.labelCount = 0;
-        label.text = self.messageField.text;
-        label.alpha = 1.0;
-    }
-}
+//# pragma mark - Send Button
+//
+//- (IBAction)onSendButtonPressed:(id)sender {
+//    NSArray *labels = @[self.chatLabel1, self.chatLabel2, self.chatLabel3, self.chatLabel4];
+//    UILabel *label;
+//    if (self.labelCount < labels.count) {
+//        label = labels[self.labelCount];
+//    }
+//    if (self.labelCount == 0) {
+//        self.labelCount += 1;
+//        label.text = self.messageField.text;
+//        label.alpha = 1.0;
+//    } else if (self.labelCount == 1) {
+//        self.labelCount += 1;
+//        label.text = self.messageField.text;
+//        label.alpha = 1.0;
+//    } else if (self.labelCount == 2) {
+//        self.labelCount += 1;
+//        label.text = self.messageField.text;
+//        label.alpha = 1.0;
+//    } else if (self.labelCount == 3) {
+//        self.labelCount = 0;
+//        label.text = self.messageField.text;
+//        label.alpha = 1.0;
+//    }
+//}
 
 #pragma mark - TableView
 
